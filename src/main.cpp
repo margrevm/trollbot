b0b73d457022fd3385895bb41d80d62e7de35c42
Philippe Mertes <pmertes@student.uliege.be>
2021-05-14 00:13:53 +0200

    Add LiquidCrystal2_IC to external lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,31 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 #include <platform/lcd/LCD.h>
+#include <elapsedMillis.h>
 
-#if defined(ARDUINO) && ARDUINO >= 100
-#define printByte(args)  write(args);
-#else
-#define printByte(args)  print(args,BYTE);
-#endif
 
 LiquidCrystal_I2C lcd(0x20, 20, 4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-
+elapsedSeconds time_elapsed;
+unsigned long current_secs;
 
 void setup() {
   Serial.begin(9600);
-  // Serial.println("test");
   // put your setup code here, to run once:
   lcd.init();                      // initialize the lcd 
   lcd.backlight();
 
   show_splash_screen();
+  current_secs = time_elapsed; 
 }
 
 void loop() {
-  
+  if (time_elapsed > current_secs) {
+    char text_buffer[20];
+    // ltoa(time_elapsed, text_buffer, 10);
+    sprintf(text_buffer, "%ul", current_secs);
+    Serial.println(current_secs);
+    current_secs = time_elapsed;
+  }
+
 }
 
